88b581ec7bfb7be11b52391b2e83452d9dd82194
Patrick José Pereira <patrickelectric@gmail.com>
2016-06-17 19:56:06 -0300

    Style corrections
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- Firmware/ControladorRobo/caliblib.h
+++ Firmware/ControladorRobo/caliblib.h
@@ -1,8 +1,8 @@
 // pinos para leitura analógica
-const int sensorPin[]={A0,A1,A2,A3,A8,A4,A5,A6,A7};
+const int sensorPin[]= {A0,A1,A2,A3,A8,A4,A5,A6,A7};
 // eeprom address for the sensors
 int addr=0;
-byte sensorLow[9] = {0}
+byte sensorLow[9] = {0};
 byte sensorHigh[9] = {1024};
 int sensorRaw[9] = {0};
 int sensor[9] = {0};
@@ -11,35 +11,35 @@ int sensor[9] = {0};
 // le os sensores e salva o valor lido de cada um no byte[9]
 void readSensors()
 {
-  for(int i=0;i<9;i++)
-      sensorRaw[i] = analogRead(sensorPin[i]);
-      sensor[i] = map(sensorRaw[i], sensorLow[i], sensorHigh[i], 0, 255);
+    for (int i=0; i<9; i++)
+        sensorRaw[i] = analogRead(sensorPin[i]);
+    sensor[i] = map(sensorRaw[i], sensorLow[i], sensorHigh[i], 0, 255);
 }
 
 // escreve os valores de sVec na eeprom para os endereços do branco
 void writeCalibWhite(byte* sVec)
 {
-  for(int i=0;i<9;i++)
-    EEPROM.write(addr+i, sVec[i]);
+    for (int i=0; i<9; i++)
+        EEPROM.write(addr+i, sVec[i]);
 }
 
 // escreve os valores de sVec na eeprom para os endereços do preto
 void writeCalibBlack(byte* sVec)
 {
-  for(int i=0;i<9;i++)
-    EEPROM.write(addr+i+9, sVec[i]);
+    for (int i=0; i<9; i++)
+        EEPROM.write(addr+i+9, sVec[i]);
 }
 
 // le os valores do branco da eeprom
 void readCalibWhite(byte* sVec)
 {
-  for(int i=0;i<9;i++)
-    sVec[i]=EEPROM.read(addr+i);
+    for (int i=0; i<9; i++)
+        sVec[i]=EEPROM.read(addr+i);
 }
 
 // le os valores de preto da eeprom
 void readCalibBlack(byte* sVec)
 {
-  for(int i=0;i<9;i++)
-    sVec[i]=EEPROM.read(addr+i+9);
+    for (int i=0; i<9; i++)
+        sVec[i]=EEPROM.read(addr+i+9);
 }
--- Firmware/ControladorRobo/hbridge.h
+++ Firmware/ControladorRobo/hbridge.h
@@ -15,20 +15,21 @@ float esquerda=1;
 float direita=1.1;
 
 
-void hBridgeSetup(){
-  // Define os pinos do acionamento da ponte H  como saida
-  pinMode(esq_f, OUTPUT);
-  pinMode(esq_t, OUTPUT);
-  pinMode(esq, OUTPUT);
-  pinMode(dir_f, OUTPUT);
-  pinMode(dir_t, OUTPUT);
-  pinMode(dir, OUTPUT);  
+void hBridgeSetup()
+{
+    // Define os pinos do acionamento da ponte H  como saida
+    pinMode(esq_f, OUTPUT);
+    pinMode(esq_t, OUTPUT);
+    pinMode(esq, OUTPUT);
+    pinMode(dir_f, OUTPUT);
+    pinMode(dir_t, OUTPUT);
+    pinMode(dir, OUTPUT);
 
     //inicia com motores desligados
-  digitalWrite(esq_t,LOW);         
-  digitalWrite(dir_t,LOW);
-  digitalWrite(esq_f,LOW);         
-  digitalWrite(dir_f,LOW);
+    digitalWrite(esq_t,LOW);
+    digitalWrite(dir_t,LOW);
+    digitalWrite(esq_f,LOW);
+    digitalWrite(dir_f,LOW);
 
 
 }
@@ -37,85 +38,88 @@ void hBridgeSetup(){
 // frente  esquerda
 void eForward(int vel)
 {
-  digitalWrite(esq_t,LOW);         
-  digitalWrite(esq_f,HIGH);         
-  analogWrite(esq,sat(vel)); 
-  
+    digitalWrite(esq_t,LOW);
+    digitalWrite(esq_f,HIGH);
+    analogWrite(esq,sat(vel));
+
 }
 
 void dForward(int vel)
 {
-  digitalWrite(dir_f,HIGH);
-  digitalWrite(dir_t,LOW);
-  analogWrite(dir,sat(vel));
-}  
-
-void goForward(int vel){
-  // frente  
-  digitalWrite(esq_t,LOW);         
-  digitalWrite(dir_t,LOW);
-  digitalWrite(esq_f,HIGH);         
-  digitalWrite(dir_f,HIGH);
-
-  analogWrite(dir,vel);
-  analogWrite(esq,vel); 
-  
+    digitalWrite(dir_f,HIGH);
+    digitalWrite(dir_t,LOW);
+    analogWrite(dir,sat(vel));
 }
-void giraEsquerda(){
-  digitalWrite(dir_t,LOW); 
-  digitalWrite(dir_f,HIGH);
-  digitalWrite(esq_t,HIGH); 
-  digitalWrite(esq_f,LOW);
-  analogWrite(dir,vel_max);  
-  analogWrite(esq,vel_max); 
-}
-void giraDireita(){
-  digitalWrite(dir_t,HIGH); 
-  digitalWrite(dir_f,LOW);
-  digitalWrite(esq_t,LOW); 
-  digitalWrite(esq_f,HIGH);
 
-  analogWrite(dir,vel_max);  
-  analogWrite(esq,vel_max); 
+void goForward(int vel)
+{
+    // frente
+    digitalWrite(esq_t,LOW);
+    digitalWrite(dir_t,LOW);
+    digitalWrite(esq_f,HIGH);
+    digitalWrite(dir_f,HIGH);
+
+    analogWrite(dir,vel);
+    analogWrite(esq,vel);
+
+}
+void giraEsquerda()
+{
+    digitalWrite(dir_t,LOW);
+    digitalWrite(dir_f,HIGH);
+    digitalWrite(esq_t,HIGH);
+    digitalWrite(esq_f,LOW);
+    analogWrite(dir,vel_max);
+    analogWrite(esq,vel_max);
+}
+void giraDireita()
+{
+    digitalWrite(dir_t,HIGH);
+    digitalWrite(dir_f,LOW);
+    digitalWrite(esq_t,LOW);
+    digitalWrite(esq_f,HIGH);
+
+    analogWrite(dir,vel_max);
+    analogWrite(esq,vel_max);
 }
 void turnRight()
 {
-  digitalWrite(esq_t,LOW);         
-  digitalWrite(dir_t,LOW);
-  digitalWrite(esq_f,HIGH);         
-  digitalWrite(dir_f,HIGH);
-  
-  analogWrite(esq,sat(vel_max));
-  analogWrite(dir,sat(vel_max*curva)); 
+    digitalWrite(esq_t,LOW);
+    digitalWrite(dir_t,LOW);
+    digitalWrite(esq_f,HIGH);
+    digitalWrite(dir_f,HIGH);
+
+    analogWrite(esq,sat(vel_max));
+    analogWrite(dir,sat(vel_max*curva));
 }
 void turnLeft()
 {
-  digitalWrite(esq_t,LOW);         
-  digitalWrite(dir_t,LOW);
-  digitalWrite(esq_f,HIGH);         
-  digitalWrite(dir_f,HIGH);
-  analogWrite(esq_f,sat(vel_max*curva));
-  analogWrite(dir_f,sat(vel_max)); 
+    digitalWrite(esq_t,LOW);
+    digitalWrite(dir_t,LOW);
+    digitalWrite(esq_f,HIGH);
+    digitalWrite(dir_f,HIGH);
+    analogWrite(esq_f,sat(vel_max*curva));
+    analogWrite(dir_f,sat(vel_max));
 }
 
 // vira em torno do próprio eixo, se tornou mais promissor em curvas muito fechadas !
 void acuteRight()
 {
-  digitalWrite(dir_t,HIGH); 
-  digitalWrite(esq_t,LOW); 
-  digitalWrite(dir_f,LOW);
-  digitalWrite(esq_f,HIGH);
-  
-  analogWrite(dir,sat(vel_max*curva_fechada));  
-  analogWrite(esq,sat(vel_max));
+    digitalWrite(dir_t,HIGH);
+    digitalWrite(esq_t,LOW);
+    digitalWrite(dir_f,LOW);
+    digitalWrite(esq_f,HIGH);
+
+    analogWrite(dir,sat(vel_max*curva_fechada));
+    analogWrite(esq,sat(vel_max));
 }
 
 void acuteLeft()
 {
-  digitalWrite(dir_t,LOW); 
-  digitalWrite(esq_t,HIGH); 
-  digitalWrite(dir_f,HIGH);
-  digitalWrite(esq_f,LOW);
-  analogWrite(esq,sat(vel_max*curva_fechada));         
-  analogWrite(dir,sat(vel_max)); 
+    digitalWrite(dir_t,LOW);
+    digitalWrite(esq_t,HIGH);
+    digitalWrite(dir_f,HIGH);
+    digitalWrite(esq_f,LOW);
+    analogWrite(esq,sat(vel_max*curva_fechada));
+    analogWrite(dir,sat(vel_max));
 }
